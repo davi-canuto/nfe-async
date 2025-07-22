baf2232aab1aef4da8b06b02c3ee1fc7fbea27a7
davi-canuto <davicanutogregorio@gmail.com>
2025-07-22 15:50:21 -0300

    initialize java microservice for sign xml correctly

---
+++ sign-xml-service/sign-xml-service/src/main/java/com/nfeasync/sign_xml_service/SignXmlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nfeasync.sign_xml_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignXmlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignXmlServiceApplication.class, args);
+	}
+
+}
---
+++ sign-xml-service/sign-xml-service/src/test/java/com/nfeasync/sign_xml_service/SignXmlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfeasync.sign_xml_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignXmlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
